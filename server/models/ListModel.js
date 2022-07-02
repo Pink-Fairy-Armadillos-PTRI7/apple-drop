dc42eb06be924fd067b8e27cfbc4ec6280f85b52
adepeju4 <adepeju.kayode@gmail.com>
2022-07-02 23:11:00 +0100

    chore(backend): story, list and auth middlewares

--- server/models/ListModel.js
+++ server/models/ListModel.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const listSchema = new Schema({
-    name: { type: String, required: true },
-    image: { type: String, required: true}
+const listItemSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  list: [
+    {
+      title: { type: String, required: true },
+      image: { type: String, required: true },
+      description: { type: String, required: true },
+    },
+  ],
 });
 
-
-
-module.exports = mongoose.model("List", listSchema);
+module.exports = mongoose.model('ListItem', listItemSchema);
--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,18 +1,14 @@
 const { Router } = require('express');
 const userController = require('../controllers/userController');
 
-
-
 const router = Router();
 
-
 router.post('/signup', userController.signUp, (req, res) => {
-    return res.status(200).json({status: 200, user: res.locals.user })
-})
+  return res.status(200).json(res.locals.user);
+});
 
 router.post('/signin', userController.signIn, (req, res) => {
-    return res.status(200).json({status:200, ...res.locals })
-})
-
+  return res.status(200).json(res.locals.user);
+});
 
 module.exports = router;
