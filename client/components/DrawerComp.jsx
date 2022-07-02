1043d6526995f73d568835c6e653c12824a4c610
imz <duv.imma@gmail.com>
2022-07-02 13:02:57 -0400

    refactor: making drawer component more responsive with navBar

--- client/components/DrawerComp.jsx
+++ client/components/DrawerComp.jsx
@@ -1,25 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import {Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material"
+import {Drawer, IconButton, List, ListItemButton, ListItemText } from "@mui/material"
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
 //import drawer component after the toolbar component in nav
 function DrawerComp () {
     //list composed of items containing primary and supplemental actions, represented by icons and text
     const [openDrawer, setOpenDrawer] = useState(false)
+    const pages = ['Register', 'Donate to Teachers']
     return ( 
         <>
          <Drawer open={openDrawer} onClose = {() => setOpenDrawer(false)}>
             <List>
-                <ListItemButton>
-                    <ListItemIcon></ListItemIcon>
-                    <ListItemText>Register</ListItemText>
-                </ListItemButton>
-                <ListItemButton>
-                    <ListItemIcon></ListItemIcon>
-                    <ListItemText>Donate to Teachers</ListItemText>
-                </ListItemButton>
+                {
+                   pages.map((page, index) => {
+                    let componentPath
+                     (index === 1)? componentPath = '/search' : componentPath = '/signup'
+                        return (
+                            <ListItemButton key={index} 
+                            component={Link} to={componentPath}
+                            >
+                            <ListItemText>{page}</ListItemText>
+                            </ListItemButton>
+                        )
+                    })
+                }
             </List>
         </Drawer>
-        <IconButton onClick ={() => setOpenDrawer(!openDrawer)}>
+        <IconButton sx= {{color: 'white', marginRight: "auto"}} onClick ={() => setOpenDrawer(!openDrawer)}>
             <MenuIcon />
         </IconButton>
         </>
