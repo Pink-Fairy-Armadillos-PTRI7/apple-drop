a7b8131f33873776b10558469febd6bbd1099ca2
imz <duv.imma@gmail.com>
2022-07-05 22:01:55 -0400

    created barebones for teacherDash

---
+++ client/components/TeacherDash.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+function TeacherDash () {
+    return ( 
+        <div className = 'teacherDash-container'>
+            <Box className = 'teacherDash-img'
+            sx ={{
+                
+            }}
+            />
+        </div>
+        
+     );
+}
+
+export default TeacherDash;
