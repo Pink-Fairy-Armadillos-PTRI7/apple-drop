b75ca0330960dd2f050cf1c77f6549ee5bfcd485
imz <duv.imma@gmail.com>
2022-07-06 21:00:24 -0400

    refactor: adding styling to teacherDashboard

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,24 +4,15 @@ import '../style.css';
 import theme from './theme';
 import Navbar from './navBar.jsx';
 import Home from './Home.jsx';
-<<<<<<< HEAD
 import TeacherDash from './TeacherDash.jsx';
-=======
-import Test from './AppTest.jsx';
->>>>>>> dev
 
 const App = () => {
   return (
     <Router>
       <Navbar theme={theme} />
       <Routes>
-<<<<<<< HEAD
         <Route exact path='/' element={<Home theme={theme} />} />
         <Route exact path = '/dashboard' element={<TeacherDash theme = {theme} />} />
-=======
-        <Route exact path="/" element={<Home theme={theme} />} />
-        <Route exact path="/test" element={<Test />} />
->>>>>>> dev
       </Routes>
     </Router>
   );
--- client/components/TeacherDash.jsx
+++ client/components/TeacherDash.jsx
@@ -1,16 +1,19 @@
-import React, {useState}from 'react';
+import React, {useState} from 'react';
 import Box from '@mui/material/Box';
 import { ThemeProvider } from '@mui/material/styles';
+
 //teacher will be directed to dashboard once they've logged in or signed up.
 function TeacherDash ({theme}) {
     return ( 
         <div className = 'teacherDash-container'>
-            <ThemeProvider>
+            <ThemeProvider theme = {theme}>
                 <Box className = 'teacherDash-img'
                     sx ={{ 
-                        
+                        display: 'flex',
+                        justifyContent: 'center',
+                        height: 200
                     }}>
-                    <h2 style = {{}}>Hello, Ms.Holubeck!</h2>
+                    <h1 style = {{color: 'white', paddingTop: '1em'}}>Hello, Ms.Holubeck!</h1>
                 </Box>
             </ThemeProvider>
         </div>
