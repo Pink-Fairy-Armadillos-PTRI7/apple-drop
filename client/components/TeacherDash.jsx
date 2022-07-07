6991405bbe2638697f19beae5d01f0084f0f6384
imz <duv.imma@gmail.com>
2022-07-06 20:39:04 -0400

    refactor: got background image of teacherDash to finally render onto page

--- client/components/TeacherDash.jsx
+++ client/components/TeacherDash.jsx
@@ -1,14 +1,18 @@
 import React, {useState}from 'react';
 import Box from '@mui/material/Box';
+import { ThemeProvider } from '@mui/material/styles';
 //teacher will be directed to dashboard once they've logged in or signed up.
-function TeacherDash () {
+function TeacherDash ({theme}) {
     return ( 
         <div className = 'teacherDash-container'>
-            <Box className = 'teacherDash-img'
-                sx ={{ 
-                }}>
-                <h2>Hello, Ms.Holubeck!</h2>
-            </Box>
+            <ThemeProvider>
+                <Box className = 'teacherDash-img'
+                    sx ={{ 
+                        
+                    }}>
+                    <h2 style = {{}}>Hello, Ms.Holubeck!</h2>
+                </Box>
+            </ThemeProvider>
         </div>
         
      );
