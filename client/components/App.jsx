745c34f4eae38d966f5d968570e67349cb13d1ac
ethanmcrae <ethanmcrae0@gmail.com>
2022-07-06 18:40:28 -0600

    Added a basic page to add items to a list

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,7 @@ import '../style.css';
 import theme from './theme';
 import Navbar from './navBar.jsx';
 import Home from './Home.jsx';
+import CreateList from './CreateList.jsx';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar theme={theme} />
       <Routes>
         <Route exact path='/' element={<Home theme={theme} />} />
+        <Route exact path='/create-list' element={<CreateList theme={theme} />} />
       </Routes>
     </Router>
   );
