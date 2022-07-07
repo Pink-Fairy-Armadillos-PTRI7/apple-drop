f38a97d36c849c001493b84af97ed2a8b48a42be
imz <duv.imma@gmail.com>
2022-07-06 21:33:07 -0400

    feat: added tabs to the teacherDashboard so teacher may navigate back and forth through all lists created

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,12 +4,8 @@ import '../style.css';
 import theme from './theme';
 import Navbar from './navBar.jsx';
 import Home from './Home.jsx';
-<<<<<<< HEAD
 import TeacherDash from './TeacherDash.jsx';
-=======
-import CreateList from './CreateList.jsx';
-import Test from './AppTest.jsx';
->>>>>>> dev
+import CreateList from './CreateList.jsx'
 
 const App = () => {
   return (
@@ -17,12 +13,9 @@ const App = () => {
       <Navbar theme={theme} />
       <Routes>
         <Route exact path='/' element={<Home theme={theme} />} />
-<<<<<<< HEAD
         <Route exact path = '/dashboard' element={<TeacherDash theme = {theme} />} />
-=======
         <Route exact path='/create-list' element={<CreateList theme={theme} />} />
-        <Route exact path="/test" element={<Test />} />
->>>>>>> dev
+        {/* <Route exact path="/test" element={<Test />} /> */}
       </Routes>
     </Router>
   );
--- client/components/TeacherDash.jsx
+++ client/components/TeacherDash.jsx
@@ -1,9 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
+import { Tabs, Tab } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 
 //teacher will be directed to dashboard once they've logged in or signed up.
 function TeacherDash ({theme}) {
+    //
+    const [value, setValue] = useState(0)
+    const handleChange = (e, newVal) => {
+        setValue(newVal)
+    }
     return ( 
         <div className = 'teacherDash-container'>
             <ThemeProvider theme = {theme}>
@@ -14,6 +20,13 @@ function TeacherDash ({theme}) {
                         height: 200
                     }}>
                     <h1 style = {{color: 'white', paddingTop: '1em'}}>Hello, Ms.Holubeck!</h1>
+                </Box>
+                <Box className ='teacher-lists'>
+                    <h3 style ={{color: theme.palette.orange.main}}>Lists</h3>
+                    <Tabs value = {value} onChange = {handleChange}>
+                        <Tab label = 'All'/>
+                        <Tab label = 'Drafts'/>
+                    </Tabs>
                 </Box>
             </ThemeProvider>
         </div>
