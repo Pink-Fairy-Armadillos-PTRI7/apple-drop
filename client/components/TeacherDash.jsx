9a32c548e78f7b2e134b7d5f55bfecd0b3eb3adf
imz <duv.imma@gmail.com>
2022-07-07 22:55:23 -0400

    working on fixing api request from backend

--- client/components/TeacherDash.jsx
+++ client/components/TeacherDash.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import { Tabs, Tab} from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
@@ -6,6 +6,8 @@ import Typography from "@mui/material/Typography";
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+
+//ULTIMATELY RELYING ON THE PERSISTENCE OF THE USER ID
 
 //TabPanel component for Tab display
 function TabPanel (props) {
@@ -29,15 +31,84 @@ function TabPanel (props) {
 }
 
 //function to fetch lists based on teacher id
-// function fetchTeacherLists () {
+async function fetchTeacherLists() {
+    //return a list containing an array of list items, object of lists?
+    const id = '62c4c0be9d30e99a23cbb26f'
+    // try {
+    //     const teacherLists = await axios.get(`/api/list/${id}`,
+    //     {
+    //         headers: {
+    //             // 'Content-Type': 'application/json',
+    //             Authorization:
+    //               'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmM0YzBiZTlkMzBlOTlhMjNjYmIyNmYiLCJlbWFpbCI6InRlc3RAMXRlc3QuY29tIiwiaWF0IjoxNjU3MjM0MzYxLCJleHAiOjE2NTczMjA3NjF9.d0GxoSf7a7Gx1GvzC8_hsjGIKsDgEu3a59dSXfjSjvQ',
+    //           }
+    //     })
+    //     // console.log('teachers are =>', teacherLists.data)
+    //     //add teacher lists to list state in teacherDash component
+    //     //add authorization header with token inside it
+    //     // setLists([response.data])
+    // }
+    // catch {
+    //     console.log('error in getting users')
+    // }
+    fetch(`/api/list/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MmM0YzBiZTlkMzBlOTlhMjNjYmIyNmYiLCJlbWFpbCI6InRlc3RAMXRlc3QuY29tIiwiaWF0IjoxNjU3MjM0MzYxLCJleHAiOjE2NTczMjA3NjF9.d0GxoSf7a7Gx1GvzC8_hsjGIKsDgEu3a59dSXfjSjvQ',
+        },
+      })
+        .then((res) => {
+          if (res.status > 399 && res.status < 200) {
+            throw new Error();
+          }
+          return res.json();
+        })
+        .then((data) => {
+            console.log('response is=>', data)
+        //   return data;
+        });
+}
+//function to fetch teacher story
+//id - coming from the 
+// async function fetchTeacherStory() {
+//     //will get object with title, image, and description properties
+//     try {
+//         const teacherStory = await axios.get('/api/myStories/:id')
+//         console.log('teachers are =>', response.data)
+//     }
+//     catch {
+//         console.log('error in getting users')
+//     }
+    
+// }
 
+// //function to fetch teacher address
+// async function fetchTeacherAddress() {
+//     //response is object containing reference to user's address
+//         //address schema has following properties: schoolName, street, city, state, postalCode
+//     try {
+//         const teacherData = await axios.get('/api/user/:id')
+//         console.log('teachers are =>', response.data)
+//     }
+//     catch {
+//         console.log('error in getting users')
+//     }
 // }
 //Renders individual list item
-// const List = () => {
-//     return (
+const List = (props) => {
+    //iterate through the teacherLists array properly 
+    const {teacherLists} = props
+    console.log('hi we are a list')
+    // teacherLists.forEach((teach) => {
+    //     console.log('we are seeing this teacher')
+    // })
+    // return (
 
-//     )
-// }
+    // )
+}
+
+
 
 
 //Teacher Dashboard Component
@@ -48,8 +119,19 @@ function TeacherDash ({theme}) {
     const handleChange = (e, newVal) => {
         setValue(newVal)
     }
-    //need to make a fetch request to get user story, school and address from backend
-    //make TabPanel component and hide and show it with current tab
+    
+    const [teacherLists, setLists] = useState([])
+    const [teacherStory, setStory] = useState({})
+    const [teacherAddress, setAddress] = useState({})
+    //useEffect to trigger getting teacher info based on id as soon as teacher component loads
+    useEffect(() =>{
+        fetchTeacherLists().catch(() => {
+            console.log()
+        })
+        // fetchTeacherStory()
+        // fetchTeacherAddress()
+    }, [])
+
     return ( 
         <div className = 'teacherDash-container'>
             <ThemeProvider theme = {theme}>
@@ -79,8 +161,13 @@ function TeacherDash ({theme}) {
                             <Tab label = 'All'/>
                             <Tab label = 'Drafts'/>
                         </Tabs>
-                        <TabPanel value = {value} index = {0}> Deez Nutz </TabPanel>
-                        <TabPanel value = {value} index = {1}> Goteem</TabPanel>
+                        <TabPanel value = {value} index = {0}> 
+                            <List></List>
+                            Deez Nutz 
+                        </TabPanel>
+                        <TabPanel value = {value} index = {1}>
+                             Goteem
+                        </TabPanel>
                     </Box>
                     <Box className ='teacher-column' id = 'teacher-story'>
                         <h3 style ={{color: theme.palette.orange.main}} >Your Story</h3>
--- server/controllers/listController.js
+++ server/controllers/listController.js
@@ -23,6 +23,7 @@ teacherList.getList = async (req, res, next) => {
   try {
     const list = await List.find({ userId: req.user._id });
     res.locals.list = list;
+    return next()
   } catch (error) {
     return next(createError({ message: { err: error.message } }));
   }
